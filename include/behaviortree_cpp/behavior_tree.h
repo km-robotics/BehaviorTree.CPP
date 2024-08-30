6d229ef8ef3992d32f662b6c323aa53584d6b6cf
Gaël Écorchard <gael@km-robotics.cz>
2024-08-30 14:46:49 +0200

    Revert "Add the AlwaysRunning node"
    This reverts commit eac3ddcad8e2afa351da3ada1996e133056fcae6.

--- include/behaviortree_cpp/behavior_tree.h
+++ include/behaviortree_cpp/behavior_tree.h
@@ -17,7 +17,6 @@
 #include "behaviortree_cpp/actions/always_failure_node.h"
 #include "behaviortree_cpp/actions/always_success_node.h"
 #include "behaviortree_cpp/actions/script_condition.h"
-#include "behaviortree_cpp/actions/always_running_node.h"
 #include "behaviortree_cpp/actions/script_node.h"
 #include "behaviortree_cpp/actions/set_blackboard_node.h"
 #include "behaviortree_cpp/actions/sleep_node.h"
