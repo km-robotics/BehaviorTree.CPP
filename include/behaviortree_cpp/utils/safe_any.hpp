55bba40dfa48ad782517375eaa643cfb30165538
Gaël Écorchard <gael@km-robotics.cz>
2024-08-30 09:44:44 +0200

    Parse true and false as boolean
    - Parse true and false as boolean.
    - Treat comparison operations as boolean.
    - Update tests accordingly.
    
    The reason for this is for compatilibity with Lua which considers anything else than `false` and `nil` as true (i.e. the integer 0 is considered true).
    
    Signed-off-by: Gaël Écorchard <gael@km-robotics.cz>

--- include/behaviortree_cpp/utils/safe_any.hpp
+++ include/behaviortree_cpp/utils/safe_any.hpp
@@ -162,6 +162,11 @@ public:
 
   Any& operator=(Any&& other) noexcept;
 
+  [[nodiscard]] bool isBool() const
+  {
+    return _original_type == typeid(bool);
+  }
+
   [[nodiscard]] inline bool isNumber() const
   {
     return _is_number;
@@ -427,6 +432,10 @@ inline void Any::copyInto(Any& dst) const
     {
       dst._any = cast<double>();
     }
+    else if (dst_type == typeid(bool))
+    {
+      dst._any = cast<bool>();
+    }
     else
     {
       throw std::runtime_error("Any::copyInto fails");
@@ -471,6 +480,14 @@ inline nonstd::expected<T, std::string> Any::stringToNumber() const
 
   const auto str = linb::any_cast<SafeAny::SimpleString>(_any);
 #if __cpp_lib_to_chars >= 201611L
+  if (str == "true")
+  {
+    return true;
+  }
+  if (str == "false")
+  {
+    return false;
+  }
   T out;
   auto [ptr, err] = std::from_chars(str.data(), str.data() + str.size(), out);
   if(err == std::errc())
